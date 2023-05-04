985cf3623ee93c4acd00eb66d163eebe053bdc8f
AJ Ezpeleta <56748201+ajezpeleta@users.noreply.github.com>
2023-05-04 18:41:44 +0800

    refactor: move `<Head>` to `_app.tsx`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import { type AppType } from "next/app";
+import Head from "next/head";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Work Life Tracker</title>
+        <meta
+          name="description"
+          content="Track your hours. Live a better life."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
